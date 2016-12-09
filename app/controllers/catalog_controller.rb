99a093a7e8433432a09848dcd82462a463dd3fb1
Axa Mei Liauw <aliauw@princeton.edu>
2016-12-09 15:29:32 -0500

    Alphabetical listing for topic and country facets. Closes #37

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -3,7 +3,7 @@ class CatalogController < ApplicationController
 
   #skip_before_filter :authenticate_user!
   skip_before_filter :authenticate_user!
-  
+
   include Blacklight::Catalog
 
   configure_blacklight do |config|
@@ -72,10 +72,10 @@ class CatalogController < ApplicationController
 
     config.add_facet_field 'resource_type_s', label: 'Format', show: false
     config.add_facet_field 'pub_date', label: 'Publication Year', single: true
-    config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 25, index_range: 'A'..'Z'
+    config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 25, index_range: 'A'..'Z', sort: 'index'
     config.add_facet_field 'language_facet', label: 'Language', limit: true
     config.add_facet_field 'lc_1letter_facet', label: 'Call Number'
-    config.add_facet_field 'subject_geo_facet', label: 'Country', limit: 10
+    config.add_facet_field 'subject_geo_facet', label: 'Country', limit: 10, sort: 'index'
     config.add_facet_field 'region_facet', label: 'Region', limit: 10
     config.add_facet_field 'subject_era_facet', label: 'Era'
 
