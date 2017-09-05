f9a257684a8afdcc9888664c83de618857537f30
Kevin Reiss <kevin.reiss@gmail.com>
2017-09-04 21:34:19 -0500

    Activate Production Env for capistrano.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 set :stage, :production
 set :rails_env, 'production'
 
-server 'libruby-dev', user: 'deploy', roles: %w{app db web}
+server 'dss-staging1', user: 'deploy', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
