193025d42476d34069d07d8a30504dd820d472b4
Shaun Ellis <shaun@sdellis.com>
2018-09-13 15:04:41 -0400

    allows for multiple lux mounting points... also some cleanup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
 import Vue from 'vue/dist/vue.esm'
 import system from 'lux-design-system'
 import 'lux-design-system/lib/system/system.css'
@@ -16,7 +15,19 @@ Vue.use(system)
 
 // create the LUX app and mount it to a wrapper with data-behavior="vue"
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '[data-behavior="vue"]'
-  })
+  // this works, but not sure about the let within the for loop for IE11
+  let elements = document.getElementsByClassName('lux')
+  for(let el of elements){
+  	new Vue({
+    	el
+    })
+  }
+
+  // this _should_ work, but is causing an issue in Rails Webpacker manifest
+  // var elements = document.getElementsByClassName('lux')
+  // for(var i = 0; i < elements.length; i++)
+  //   new Vue({
+  //     el: elements[i]
+  //   })
+  // }
 })
