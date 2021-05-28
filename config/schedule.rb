13d672ed4bf3937fb67ef8e8e8de2c89d3f36564
Anna Headley <anna.headley@gmail.com>
2021-05-28 11:41:36 -0400

    Add rake task to expire guest users and schedule it in cron
    fixes #180

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,10 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+set :job_template, "bash -l -c 'export PATH=\"/usr/local/bin/:$PATH\" && :job'"
+
+job_type :logging_rake, 'cd :path && :environment_variable=:environment bundle exec rake :task :output'
+
+every 1.day, at: '11pm', roles: [:db] do
+  logging_rake 'dss:clean:guest_users', output: { error: '/tmp/clean_guest_users.log', standard: '/tmp/clean_guest_users.log' }
+end
