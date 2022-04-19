fec63f78f969303dc1ba0de85414a56f4d236ef3
Jane Sandberg <js7389@princeton.edu>
2022-04-18 17:30:05 -0700

    Add a few data seeds; remove old seeds directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-require 'csv'
-### Create new seed routine
-
-puts "Seed routine forthcoming"
+[
+  {country: 'Indonesia', title: 'Indonesia - Census of Agriculture 2003'},
+  {county: 'Libya', title: 'The effect of settler farming on indigenous agriculture: Evidence from Italian Libya'},
+  {country: 'Spain', title: 'Land reform and peasant revolution: Evidence from 1930s Spain'},
+  {country: 'Uganda', title: 'Orange Fleshed Sweet Potatoes 2014, Baseline Survey [Uganda]'}
+].each do |resource|
+  Resource.create(name: resource[:title],
+                  countries: [Country.create(name: resource[:country])],
+                  resource_type: 'Single study')
+end
