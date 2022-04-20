ca91970e103025bada2d0cb3556fe1669577b0f7
Jane Sandberg <sandbergja@users.noreply.github.com>
2022-04-20 07:48:12 -0700

    Fix typo
    Co-authored-by: Christina Chortaria <actspatial@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 [
   {country: 'Indonesia', title: 'Indonesia - Census of Agriculture 2003'},
-  {county: 'Libya', title: 'The effect of settler farming on indigenous agriculture: Evidence from Italian Libya'},
+  {country: 'Libya', title: 'The effect of settler farming on indigenous agriculture: Evidence from Italian Libya'},
   {country: 'Spain', title: 'Land reform and peasant revolution: Evidence from 1930s Spain'},
   {country: 'Uganda', title: 'Orange Fleshed Sweet Potatoes 2014, Baseline Survey [Uganda]'}
 ].each do |resource|
