1582ea4e8c93ea564a62692bddbc08e07ca3ebff
Jane Sandberg <js7389@princeton.edu>
2022-05-09 11:32:53 -0700

    Display the tools on the show page again

--- spec/system/show_page_spec.rb
+++ spec/system/show_page_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe 'Show page', type: :system do
     visit '/catalog/resource3'
     expect(page).to have_text('Single study')
   end
+
+  it 'has a bookmark checkbox' do
+    visit '/catalog/resource3'
+    expect(page).to have_selector('#toggle-bookmark_resource3')
+  end
 end
