0f67f17ef23c598f5edd1a3facaacecdc593394f
Max Kadel <mkadel@princeton.edu>
2023-01-27 09:17:00 -0500

    Default to running system specs headless

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,13 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.before(:each, type: :system) do
+    if ENV["RUN_IN_BROWSER"]
+      driven_by(:selenium_chrome)
+    else
+      driven_by(:selenium_chrome_headless)
+    end
+  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
