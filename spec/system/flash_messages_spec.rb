b20cc8d950f5a2809a562076bcb75f184a92503f
Christina Chortaria <actspatial@gmail.com>
2023-02-01 17:24:48 -0500

    Test that clicking on the flash message will remove the flash message Update headless screen size
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- spec/system/flash_messages_spec.rb
+++ spec/system/flash_messages_spec.rb
@@ -5,11 +5,14 @@ require 'rails_helper'
 RSpec.describe 'Flash messages', type: :system do
   it 'has close button' do
     visit '/catalog'
-    login_as FactoryBot.create(:user)
+    user1 = FactoryBot.create(:user, username: 'username1')
+    login_as user1
     visit '/catalog'
-    click_button('User')
+    click_button(user1.username)
     click_link('Log Out')
     visit '/catalog'
     expect(page).to have_text('Signed out successfully.')
+    find('#main-flashes > div > div > button-tag > span').click
+    expect(page).to have_no_content('Signed out successfully.')
   end
 end
