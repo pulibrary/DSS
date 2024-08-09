2aae7289f3a6ea0e4f1ece9a1cfc5339d244cd56
Kevin Reiss. <kevin.reiss@gmail.com>
2024-08-09 17:11:48 -0500

    Update root redirect; remove redirects that are no longer needed.

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -3,17 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'DSS root route' do
-  context 'when user visits the staging site' do
-    before { host! 'dss-staging.princeton.edu' }
-
-    it 'redirects to the staging drupal site' do
-      get '/'
-      expect(response).to redirect_to('https://library-staging.princeton.edu/dss')
-    end
-  end
-
-  it 'redirects to the production drupal site' do
+  it 'redirects to the DSS home page' do
     get '/'
-    expect(response).to redirect_to('https://library.princeton.edu/dss')
+    expect(response).to redirect_to('https://library.princeton.edu/services/data-and-statistical-services')
   end
 end
