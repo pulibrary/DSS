bc6ae793ad2ab9f047f006bdbf36ac32237ce81f
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-08-09 10:00:37 -0400

    Add health check

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,8 @@
 require 'factory_bot'
 FactoryBot.find_definitions
 
+require 'webmock/rspec'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
