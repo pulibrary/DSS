a506f490738b6f08de0256c68801dbae20ce2d79
Max Kadel <mkadel@princeton.edu>
2024-08-09 09:59:35 -0400

    Address deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ Bundler.require(*Rails.groups)
 
 module DssCatalog
   class Application < Rails::Application
+    config.active_support.cache_format_version = 7.0
+    config.load_defaults 6.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
