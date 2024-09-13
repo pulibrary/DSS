0c1feedb08eb699a9c4761abaf6b2a21f11f5afd
Jane Sandberg <sandbergja@users.noreply.github.com>
2024-09-12 17:10:34 -0700

    Turn on filename annotations for rendered views in development mode (#475)
    This adds comments to the rendered HTML saying which partials
    and view components were used, similar to what we have in
    Orangelight.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = false
 
+  config.action_view.annotate_rendered_view_with_filenames = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
